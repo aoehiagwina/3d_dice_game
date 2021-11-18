2fa8fed7bef50d7d8842e6ad5dc129c8afa93e5d
aoehiagwina <a.o.ehiagwina@gmail.com>
2021-11-18 15:24:54 +0000

    first update on single player score and title

--- dice_images/dice_single_player.js
+++ dice_images/dice_single_player.js
@@ -3,8 +3,9 @@ const dice_cube = document.querySelector(".dice");
 const roll_button = document.getElementById("roll");
 let score = 0;
 let currentClass = "";
-let player_name = document.getElementById("gamer").value;
+const player_name = document.getElementById("gamer");
 const submit = document.getElementById("Submit");
+const scoring = document.getElementById("player1_score");
 
 
 function getRandomInt(min, max) {
@@ -14,27 +15,24 @@ function getRandomInt(min, max) {
   //The maximum is exclusive and the minimum is inclusive
 }
 
-const player = {
-  name: player_name,
-  point: score,
-  
-  win : function (){if (score > 21){
-      console.log(`${this.name} Wins`);}
-  },
-}; 
-
-submit.addEventListener('click',() => {
-  document.getElementById("player1_name").innerHTML = player.name;});
-
-
-
-
 // on click eventlistener for the button element
 roll_button.addEventListener('click', rollDice);
 
+submit.addEventListener('click',() => {
+  document.getElementById("player1_name").innerHTML = player_name.value;
+  player_name.value = ""
+  score = 0;
+  scoring.innerHTML = `Score = ${score}`;
+});
+
+
 function rollDice(){
   let randNum = getRandomInt(1,7); 
-  score += randNum;
+  score = score + randNum;
+  checkResult()
+  if(score > 0){
+    scoring.innerHTML = `Score = ${score}`;
+  }
   let showScore = 'show-' + randNum;
   dice_cube.showScore;
   console.log(showScore);
@@ -47,3 +45,11 @@ function rollDice(){
   currentClass = showScore;
 }
 rollDice();
+
+function checkResult(){
+  if (score > 21){
+    document.getElementById("player_result").innerHTML =`${player_name.value} Won`;
+  }
+}
+
+
